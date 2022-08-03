405c3e288199cd394ebf3e0e609a5324d0de32bb
Miguel de  Oliveira <migvanderlei@gmail.com>
2022-08-03 01:01:32 -0400

    feat: routes definition

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,17 +1,38 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+import Blockchain from "./infra/blockchain/Blockchain";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Transport Register App');
+app.get("/envio", (req: Request, res: Response) => {
+  res.send("Transport Register App");
 });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.post("/envio", (req: Request, res: Response) => {
+  res.send("Transport Register App");
+});
+
+app.put("/envio", (req: Request, res: Response) => {
+  res.send("Transport Register App");
+});
+
+app.get("/envios", (req: Request, res: Response) => {
+  res.send("Transport Register App");
+});
+
+app.get("/blockchain", (req: Request, res: Response) => {
+  const blockchain = Blockchain.getInstance();
+
+  if (blockchain.isValidBlockchain()) {
+    res.send('<p> Blockchain is <b>valid</b>! </p><br/>' + blockchain);
+  } else {
+    res.send('<p> Blockchain is <b>invalid</b>! </p><br/>' + blockchain)
+  }
+
 });
 
 app.listen(port, () => {
