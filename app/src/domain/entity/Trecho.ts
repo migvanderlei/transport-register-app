f85de7cc2ede0f0c4a540e324f6928b803441fb2
Miguel de  Oliveira <migvanderlei@gmail.com>
2022-08-04 01:07:24 -0400

    fix: removing unnecessary params

--- app/src/domain/entity/Trecho.ts
+++ app/src/domain/entity/Trecho.ts
@@ -2,23 +2,17 @@ import Endereco from "./Endereco";
 import Pacote from "./Pacote";
 
 export default class Trecho {
-  hash: string;
-  pacote: Pacote;
   origem: Endereco;
   destino: Endereco;
   destinoFinal: Endereco;
   observacoes?: string;
 
   constructor(
-    hash: string,
-    pacote: Pacote,
     origem: Endereco,
     destino: Endereco,
     destinoFinal: Endereco,
     observacoes?: string
   ) {
-    this.hash = hash;
-    this.pacote = pacote;
     this.origem = origem;
     this.destino = destino;
     this.destinoFinal = destinoFinal;
